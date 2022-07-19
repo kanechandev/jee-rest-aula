93104052dbafe69160fe523f3de99c1a4e040e2c
cursoskanechan <kanes.chan@gmail.com>
2022-07-19 09:22:20 -0300

    Filtro no head.

---
+++ src/main/java/rest/exer14/Servidor.java
@@ -0,0 +1,25 @@
+package rest.exer14;
+
+import java.net.URI;
+
+import javax.ws.rs.core.UriBuilder;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+public class Servidor {
+
+	public static void main(String[] args) {
+		try {
+			URI uri = UriBuilder.fromUri("http://localhost").port(8080).build();
+			ResourceConfig config = new ResourceConfig();
+			config.packages("rest.exer14");
+			HttpServer server = GrizzlyHttpServerFactory.createHttpServer(uri, config);
+			System.out.println("Servidor está levantado..."+server);
+		} catch (Exception e) {
+			System.out.println("Error ao iniciar o servidor: "+e.getMessage());
+		}
+	}
+
+}
